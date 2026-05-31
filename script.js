7f4c98661d5fdc0e83a25313e7486752e089cccc
Subphoria <loanne.subphoria@gmail.com>
2026-05-31 23:06:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,44 +6,44 @@ let score = 0;
 let questions = [];
 let gameHistory = [];
 
-// Dictionnaire avec les NOMS EXACTS des fichiers locaux
+// Dictionnaire corrigé avec les noms exacts de tes fichiers locaux
 const fileMap = {
-    "M_1": "Paris_transit_icons_-Métro_1.svg",
-    "M_2": "Paris_transit_icons-Métro_2.svg",
-    "M_3": "Paris_transit_icons-Métro_3.svg",
-    "M_3bis": "Paris_transit_icons-Métro_3bis.svg",
-    "M_4": "Paris_transit_icons-Métro_4.svg",
-    "M_5": "Paris_transit_icons-Métro_5.svg",
-    "M_6": "Paris_transit_icons-Métro_6.svg",
-    "M_7": "Paris_transit_icons-Métro_7.svg",
-    "M_7bis": "Paris_transit_icons-Métro_7bis.svg",
-    "M_8": "Paris_transit_icons-Métro_8.svg",
-    "M_9": "Paris_transit_icons-Métro_9.svg",
-    "M_10": "Paris_transit_icons-Métro_10.svg",
-    "M_11": "Paris_transit_icons-Métro_11.svg",
-    "M_12": "Paris_transit_icons-Métro_12.svg",
-    "M_13": "Paris_transit_icons-Métro_13.svg",
-    "M_14": "Paris_transit_icons-Métro_14.svg",
-    "RER_A": "Paris_transit_icons-RER_A.svg",
-    "RER_B": "Paris_transit_icons-RER_B.svg",
-    "RER_C": "Paris_transit_icons-RER_C.svg",
-    "RER_D": "Paris_transit_icons-RER_D.svg",
-    "RER_E": "Paris_transit_icons-RER_E.svg",
-    "T_1": "Paris_transit_icons-Tram_1.svg",
-    "T_2": "Paris_transit_icons-Tram_2.svg",
-    "T_3a": "Paris_transit_icons-Tram_3a.svg",
-    "T_3b": "Paris_transit_icons-Tram_3b.svg",
-    "T_4": "Paris_transit_icons-Tram_4.svg",
-    "T_5": "Paris_transit_icons-Tram_5.svg",
-    "T_6": "Paris_transit_icons-Tram_6.svg",
-    "T_7": "Paris_transit_icons-Tram_7.svg",
-    "T_8": "Paris_transit_icons-Tram_8.svg",
-    "T_9": "Paris_transit_icons-Tram_9.svg",
-    "T_10": "Paris_transit_icons-Tram_10.svg",
-    "T_11": "Paris_transit_icons-Tram_11.svg",
-    "T_12": "Paris_transit_icons-Tram_12.svg",
-    "T_13": "Paris_transit_icons-Tram_13.svg",
-    "T_14": "Paris_transit_icons-_Tram_14.svg"
+    "M_1": "Paris_transit_icons_-_Métro_1.svg",
+    "M_2": "Paris_transit_icons_-_Métro_2.svg",
+    "M_3": "Paris_transit_icons_-_Métro_3.svg",
+    "M_3bis": "Paris_transit_icons_-_Métro_3bis.svg",
+    "M_4": "Paris_transit_icons_-_Métro_4.svg",
+    "M_5": "Paris_transit_icons_-_Métro_5.svg",
+    "M_6": "Paris_transit_icons_-_Métro_6.svg",
+    "M_7": "Paris_transit_icons_-_Métro_7.svg",
+    "M_7bis": "Paris_transit_icons_-_Métro_7bis.svg",
+    "M_8": "Paris_transit_icons_-_Métro_8.svg",
+    "M_9": "Paris_transit_icons_-_Métro_9.svg",
+    "M_10": "Paris_transit_icons_-_Métro_10.svg",
+    "M_11": "Paris_transit_icons_-_Métro_11.svg",
+    "M_12": "Paris_transit_icons_-_Métro_12.svg",
+    "M_13": "Paris_transit_icons_-_Métro_13.svg",
+    "M_14": "Paris_transit_icons_-_Métro_14.svg",
+    "RER_A": "Paris_transit_icons_-_RER_A.svg",
+    "RER_B": "Paris_transit_icons_-_RER_B.svg",
+    "RER_C": "Paris_transit_icons_-_RER_C.svg",
+    "RER_D": "Paris_transit_icons_-_RER_D.svg",
+    "RER_E": "Paris_transit_icons_-_RER_E.svg",
+    "T_1": "Paris_transit_icons_-_Tram_1.svg",
+    "T_2": "Paris_transit_icons_-_Tram_2.svg",
+    "T_3a": "Paris_transit_icons_-_Tram_3a.svg",
+    "T_3b": "Paris_transit_icons_-_Tram_3b.svg",
+    "T_4": "Paris_transit_icons_-_Tram_4.svg",
+    "T_5": "Paris_transit_icons_-_Tram_5.svg",
+    "T_6": "Paris_transit_icons_-_Tram_6.svg",
+    "T_7": "Paris_transit_icons_-_Tram_7.svg",
+    "T_8": "Paris_transit_icons_-_Tram_8.svg",
+    "T_9": "Paris_transit_icons_-_Tram_9.svg",
+    "T_10": "Paris_transit_icons_-_Tram_10.svg",
+    "T_11": "Paris_transit_icons_-_Tram_11.svg",
+    "T_12": "Paris_transit_icons_-_Tram_12.svg",
+    "T_13": "Paris_transit_icons_-_Tram_13.svg",
+    "T_14": "Paris_transit_icons_-_Tram_14.svg"
 };
 
 let allTerminus = [];
@@ -119,15 +119,15 @@ async function loadData() {
     }
 }
 
+// Fonction corrigée : le lien vers le fichier local doit utiliser le chemin exact
 function getLogoHTML(val, type, isLarge = false) {
     val = val.toString().trim();
     let key = `${type}_${val}`;
     let sizeClass = isLarge ? 'logo-large' : 'logo-small';
     
     if (fileMap[key]) {
-        // Lien direct vers le dossier, sans encodage forcé qui cassait le texte
+        // On ne change pas le nom de fichier ici pour être sûr qu'il corresponde au système de fichiers Windows/Mac
         let url = `liste_logos_lignes/${fileMap[key]}`;
-        
         return `<img src="${url}" class="transit-logo ${sizeClass}" alt="Ligne ${val}" onerror="this.style.display='none'; this.nextElementSibling.style.display='inline-block';" /><span style="display:none; font-weight:bold;">${type} ${val}</span>`;
     } else {
         return `<span style="font-weight:bold;">${type} ${val}</span>`;
